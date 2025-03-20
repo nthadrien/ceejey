923679f2f1b0c8f34d1b5b0f27bbc8cef9c99757
nthadrien <hadrien.wakacode@gmail.com>
2025-03-20 14:39:20 +0100

    trying

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './src/remark-reading-time.mjs';
 // solid js 
 import solidJs from '@astrojs/solid-js';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://stargazers.club',
-  site: 'https://nthadrien.github.io/ceejey-ssg/',
+  site: 'https://nthadrien.github.io',
+  base: '/ceejey-ssg/',
   // markdown settings
   markdown: {
       remarkPlugins: [
         remarkReadingTime,
       ]
   },
-  integrations: [solidJs({ devtools: true }), sitemap()],
+  integrations: [
+    solidJs({ devtools: true }), 
+    // sitemap()
+  ],
 });
